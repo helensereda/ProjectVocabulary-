905f38259c27f0257e6f007dc530195767a26df7
helensereda <sereda.elenaby@gmail.com>
2023-06-20 11:24:58 +0300

    INCOMPLETE HOMEWORK

--- src/main/java/com/example/peoples_name/HelloApplication.java
+++ src/main/java/com/example/peoples_name/HelloApplication.java
@@ -1,7 +1,6 @@
 
 
 package com.example.peoples_name;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,8 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 300, 400);
-        stage.setTitle("Main");
+        Scene scene = new Scene(fxmlLoader.load(), 300, 420);
+        stage.setTitle("LatCheck");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
--- src/main/java/com/example/peoples_name/HelloController.java
+++ src/main/java/com/example/peoples_name/HelloController.java
@@ -1,30 +1,38 @@
 
 package com.example.peoples_name;
 
-import javafx.event.ActionEvent;
-import javafx.scene.control.ListView;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class HelloController {
+   public TextArea theWord;
+    public TextArea translation;
+    private Map<String, String> voc = new TreeMap<>();
+    private String tempKey;
 
-    public ListView listView = new ListView<>();
+    public HelloController() {
 
-    public void action(ActionEvent actionEvent) throws IOException {
-
-  Scanner scaner = new Scanner(new FileReader("src/main/resources/com/example/peoples_name/worlds.txt"));
-
-        String worlds;
-
-        while (scaner.hasNext()){
-            worlds = scaner.next();
-            listView.getItems().add(worlds);
+        File path = new File("src\\main\\resources\\com\\example\\peoples_name/worlds.txt");
+        try {
+            Scanner read = new Scanner(path);
+            while (read.hasNext()) {
+                String[] temp = read.nextLine().split(" ", 2);
+                voc.put(temp[0], temp[1]);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        System.out.println(voc);
+        tempKey = "";
     }
+
 }
 
 
